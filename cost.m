5a3dc3754a411fe0a7bf0909509375feb28b1c35
ollin23 <balam.ariya@gmail.com>
2019-11-05 00:32:24 -0800

    update cost.m
    Updated for use with oneHotEncoding function. Additional functionality by means of more cost/loss functions.

--- cost.m
+++ cost.m
@@ -1,30 +1,33 @@
-function [c] = cost(func, prediction, actual, targetMatrix)
+function [J] = cost(func, prediction, actual)
+%cost returns the cost/loss depending on the activation function
+%
+% func : the type of cost/loss function; string input
+% options -
+%   'cross'
+%       :: calculates the cross-entropy of between the prediction data and
+%          the labeled data
+%   'hinge'
+%       :: hinge loss used for classifiers; typically used for support
+%          vector machines (SVMs), but applicable elsewhere
+%   'KL'
+%       :: KullBack-Leibler Divergence, also called relative entropy
+%          is a measure of how the prediction probability distribution differs
+%          from the labeled data
+%   default: MSE (mean squared error)
+%       :: calculates the mean squared error the prediction data and
+%          the labeled data
 
-[cols, ~] = size(targetMatrix);
-
-rows = int32(length(prediction)/ cols);
-
-prediction = reshape(prediction,rows,cols);
-
-output = [];
-for i = 1:length(actual)
-    if actual(i) == 0
-        output(i,:) = targetMatrix(10,:);
-    else
-        output(i,:) = targetMatrix(actual(i),:);
-    end
-end
+n = length(prediction);
 
 switch func
     case 'cross'
-        n = length(prediction);
-        
-        c = -(1/n) * sum(output.*log(prediction)+...
-            (1 - output).*log(1-prediction));
-
-    case 'mse'
-        c = .5 * sum(prediction - output).^2;
-    otherwise
+        J = -sum(actual .* log(prediction)) / n;
+    case 'hinge'
+        J = max(0, 1-prediction .* actual);
+    case 'KL'
+        J = sum(prediction .* log(prediction ./ actual));
+    otherwise % default is MSE
+        J = (sum(prediction - actual).^2) / n;
 end
 
 end
