50ea8dd40050bec98fc97ae29e0cee749aa37d5f
ollin23 <balam.ariya@gmail.com>
2019-11-05 00:36:48 -0800

    updated createNetwork.m
    - changed error initialization to n x n matrix
    - additional comments

--- createNetwork.m
+++ createNetwork.m
@@ -1,17 +1,23 @@
-function [weights, bias, errors] = createNetwork(layers)
+function [weights, bias, error] = createNetwork(layers)
 %createNetwork produces a fully connected a neural net
+%
 % weights : the network 
 %
 %
+
+% seed the RNG with Mersenne Twister and a seed of 1 for reproducibility
 rng(1,'twister');
 weights = {};
 bias = [];
 
+% designate the weight to each node in each layer with a random real number
+% from the uniform distribution between (0,1) then halved
 for i = 1:(length(layers)-1)
     weights{1,i} = randn([layers(i+1) layers(i)]) *.5;
     bias(i) = 1;
-    errors{1,i} = ones(1,layers(i));
 end
 
+% create error layer; this layer will be the initial input for backprop
+error = ones(layers(end), layers(end));
 
 end
