8cc005dc8d3fc23ec206db5d5e641e71d55ce513
ollin23 <balam.ariya@gmail.com>
2019-11-05 00:39:04 -0800

    update softmax.m
    Altered function to increase numeric stabilization. Additional commentary.

--- softmax.m
+++ softmax.m
@@ -1,6 +1,16 @@
 function [a] = softmax(h)
 %SOFTMAX takes vector h and transforms it into a probability in range [0,1]
+%
+% variables:
+% h - the input matrix; the hypothesis generated from previous layers via
+%     the weights * input + bias (W'x + b)
+% ex - created for numerical stabilization. This precludes overflow and
+%     prevents division by zero from negatives with obscenely large 
+%     exponents
+% a - the activated data; used as output data to compare/constrast with
+%     labeled data
 
-    a = exp(h)/ sum(exp(h));
+    ex = exp(h - max(h));
+    a = ex / sum(exp(ex));
 
 end
