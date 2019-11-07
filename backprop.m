1e490c3408a7d6f5dd62e4f61fad4257e56fea94
ollin23 <balam.ariya@gmail.com>
2019-11-07 01:38:10 -0800

    updated backprop
    added documentation; improved performance

--- backprop.m
+++ backprop.m
@@ -1,9 +1,14 @@
-function [] = backprop(layers, batchEncodedLabels, prediction, batch)
+function [] = backprop(batchEncodedLabels, prediction, batch, nn)
 %backprop executes the backpropagation algorithm on the network
-%
+%PARAMETERS
+% layers - number of layers for the algorithm to propagate through
+% batchEncodedLabels - the target labels for the current batch
+% prediction - the predicted result
+% batch - reference for the current batch
+% nn - the neural network
 %
     b = batch;
-    
+    layers = length(nn.weights);
     % move backwards through the layers
     for i = layers:-1:1
         if i == layers
