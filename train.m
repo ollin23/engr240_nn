7384a6b1ec94a2c08df4b584a66f4d97feeb4ad7
ollin23 <balam.ariya@gmail.com>
2019-11-08 09:06:12 -0800

    updated documentation, minor changes

--- train.m
+++ train.m
@@ -1,57 +1,68 @@
-function [J] = train(images, encodedLabels, nn)
+function train(self)
 %train executes feedforward, backpropagation, and updates the biases and
 %weights of neural network
 
-    samples = length(images);
-    batchSize = ceil(samples / nn.batches);
-    layers = length(nn.weights);
-    J = [];
+    % size of the sample set
+    sampleSize = length(self.images);
+    
+    % determine batch size
+    % If batchSize = 1, then 
+    batchSize = ceil(sampleSize / self.batches);
+    layers = length(self.weights);
+    
+    % allocate space for error and accuracy
+    J = 0;
 
-    for batch = 1:nn.batches
-        nn.memory = {};
+    % Start the minibatches. If there is only one batch, there will only be
+    % one loop
+    for batch = 1:self.batches
+        % clear out the memory of previous batches
+        self.memory = {};
 
         % designate start and end points for batch
-        start = (batch * batchSize - batchSize) + 1;
-        stop = (batch * batchSize);
+        start = (batch * batchSize) - batchSize + 1;
+        stop = batch * batchSize;
         range = start:stop;
 
         % pull samples, encoded labels, and numeric labels
-        batchSample = images(range,:);
-        batchEncodedLabels = encodedLabels(range,:);
+        batchSample = self.images(range,:);
+        batchEncodedLabels = self.encodedLabels(range,:);
 
-        % for each sample in batch
+        % Cycle through each sample in the batch
         for b = 1:batchSize
             input = batchSample(b,:);
 
-            % process each sample through every neural layer
+            % Feedforward
+            % Process each sample through every neural layer
             for i = 1:layers 
-                h = feedforward(i, nn.weights, input, nn.bias);
+                h = feedforward(i, self.weights, input, self.bias);
 
-                % use different transfer function for last layer
+                % Use different transfer function for last layer
                 if i == layers
-                    func = nn.lastLayer;
+                    func = self.lastLayer;
                 else
-                    func = nn.transfer;
+                    func = self.transfer;
                 end
                 a = activate(h,func,false);
 
                 % save data in memory
-                nn.memory{b,i} = a;
+                self.memory{b,i} = a;
 
-                % use processed data for input in the next layer
+                % store output data as input in the next layer
                 input = a;
             end
         end
 
-        % calculate the error at end of batch
-        [rows, cols] = size(nn.memory);
-        prediction = reshape([nn.memory{:,cols}],...
+        % calculate error at end of batch
+        [rows, cols] = size(self.memory);
+        prediction = reshape([self.memory{:,cols}],...
             length(batchEncodedLabels),rows)';
 
-        J = [J; mean(cost(nn.costFunction, prediction, batchEncodedLabels))];
-        nn.errors = [nn.errors; J];
+        J = sum(cost(self.costFunction, prediction, batchEncodedLabels));
+        self.accuracy = sum(dot(prediction,batchEncodedLabels));
+        self.errors = [self.errors; J, self.accuracy];
 
         % backpropagation
-        backprop(batchEncodedLabels, prediction, b, nn);
+        backprop(input, batchEncodedLabels, prediction, b, self);
     end
 end
