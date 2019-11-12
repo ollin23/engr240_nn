82af312f4ee1bdce788321aed300c66a353aaab4
ollin23 <balam.ariya@gmail.com>
2019-11-12 11:46:33 -0800

    massive update

--- backprop2.m
+++ backprop2.m
@@ -1,79 +1,42 @@
-function [weightDelta, biasDelta] = backprop2(self, input)
+function [weightDelta, biasDelta] = backprop2(self, prediction, target, input)
 
 layers = length(self.weights);
-
-%gradient for output layer
-h = self.memory{layers};
-gradient = (activate(h,self.lastLayer,true));
 
 weightDelta = cell(1,layers);
 biasDelta = zeros(layers);
 
-
 % gradient for hidden layers
-for i = layers:-1:1
+for layer = layers:-1:1
 
-    W = self.weights{i};
-    
-    % retrieve previous layer's non-activated output
-    if (i-1) == 0
-        h = input;
-        h1 = 0;
+    h = self.memory{layer};
+        
+    % last to output layer is different
+    if layer == layers
+        gradient = prediction - target;
+        h = self.memory{layer-1};
+        a = activate(h,self.transfer,true);
     else
-        h = self.memory{i};
-        h1 = self.memory{i-1};
-    end
-
-    % calculate derivatives
-    da = (activate(h,self.transfer,true));
-
-    % calculate gradient
-    if i == 1
-        deltaBias = mean(mean(gradient') * self.bias(i));
-        gradient = gradient' * da;
-    else
-        if size(gradient) == size(da)
-
-        elseif size(gradient) < size(da)
-            [x] = abs(size(gradient) - size(da));
-            gradient = cat(1,repmat(gradient,x(1),1),gradient);
+        gradient = gradient * self.weights{layer+1};
+        gradient = activate(h,self.transfer,true) .* gradient;
+        if layer-1 == 0
+            a = input;
         else
-            [x] = abs(size(da) - size(gradient));
-            da = cat(1,repmat(da,x(1),1),da);
+            a = activate(self.memory{layer-1},self.transfer,false);
         end
-        gradient = gradient .* da;
-        deltaBias = mean(mean(gradient) * self.bias(i));
     end
     
-    % apply optimization feature
+    delta = gradient' * a;
+    deltaBias = mean(mean(gradient') * self.bias(layer));
+    
     switch self.optimization
         case 'none'
-            delta = self.eta * gradient'*h1;
+            delta = self.eta * delta;
+            deltaBias = self.eta * deltaBias;
     end
-
-    % gradient for next layer
-    if i ~= 1
-        gradient = (gradient * W);
-        
-    % make sure the delta is facing the right direction for the last layer
-    else
-        delta = delta';
-    end
-   
-    % output deltas for update
-    weightDelta{i} = delta;
-    biasDelta(i) = deltaBias;
     
-end % end of layers
-    
-    % calculate multiple summations
-    function [y] = allsum(x)
-        y = sum(x);
-        if length(y) == 1
-            y = y;
-        else
-            y = allsum(y);
-        end
-    end
-   
+    weightDelta{layer} = -delta;
+    biasDelta(layer) = -deltaBias;
 end
+    
+end % end of function
+    
