78c407a4f8a43bd988997dbb823fca099e0d03e8
ollin23 <balam.ariya@gmail.com>
2019-11-12 22:42:54 -0800

    added optimization options

--- backprop2.m
+++ backprop2.m
@@ -28,14 +28,31 @@ for layer = layers:-1:1
     delta = gradient' * a;
     deltaBias = mean(mean(gradient') * self.bias(layer));
     
-    switch self.optimization
-        case 'none'
-            delta = self.eta * delta;
-            deltaBias = self.eta * deltaBias;
+    % * * * * * * * * 
+    % OPTIMIZATIONS
+    % * * * * * * * * 
+    delta = self.eta * delta;
+    deltaBias = self.eta * deltaBias;
+    if ~(self.optim.none)
+        if self.optim.lasso
+            delta = delta +...
+                self.lambda * norm(self.weights{layer}) / length(self.labels);
+        end
+        if self.optim.ridge
+            delta = delta + ...
+                self.lambda * norm(self.weights{layer}.^2) / length(self.labels);
+        end
+        if self.optim.momentum
+            delta = delta + self.mu * self.oldDeltas{layer};
+        end
     end
     
-    weightDelta{layer} = -delta;
-    biasDelta(layer) = -deltaBias;
+    
+    weightDelta{layer} = delta;
+    biasDelta(layer) = deltaBias;
+    
+    % store delta for use with momentum in next sample
+    self.oldDeltas{layer} = delta;
 end
     
 end % end of function
