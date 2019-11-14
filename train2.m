570144ce05a19ebc15ef0adb56c3bc075cbca94d
ollin23 <balam.ariya@gmail.com>
2019-11-13 16:11:46 -0800

    added optimizations

--- train2.m
+++ train2.m
@@ -3,12 +3,14 @@ function train2(self)
 % weights of neural network
 
     % size of the sample set
-    sampleSize = length(self.images);
+    [sampleSize,~] = size(self.images);
 
     % determine batch size
     % If batchSize == 1 or 0, then batchSize = 1
     if self.batches == 1 || self.batches == 0
-        batchSize = sampleSize;        
+        batchSize = sampleSize;
+    elseif self.batches > sampleSize
+        batchSize = sampleSize;
     else
         batchSize = ceil(sampleSize / self.batches);
     end
@@ -19,8 +21,14 @@ function train2(self)
     newWeights = cell(batchSize, layers);
     newBias = zeros(batchSize,layers);
     
+    k = 2;
+    if sampleSize > 1000
+        k = sampleSize / 1000;
+    end
+    counter = 0;
     % begin cycling through the samples
     for sample = 1:sampleSize
+        counter = counter + 1;
         self.memory = {};
         input = self.images(sample,:);
         target = self.encodedLabels(sample,:);
@@ -48,44 +56,44 @@ function train2(self)
         [rows, cols] = size(self.memory);
         prediction = reshape([self.memory{:,cols}],[],rows)';
 
-        J = J + sum(cost(self.costFunction, prediction, target)) / sample;
-        
-        self.accuracy = sum(dot(prediction,target)) / length(self.accuracy);
-        self.errors = [self.errors, J];
-    
-        %fprintf('\nSample : %d\n',sample);
-        % backprop
-        [w, b] = backprop2(self, self.images(sample,:));
+        J = cost('cross', prediction, target);
+        acc = dot(target,prediction);
+                    
+        self.accuracy = cat(1,acc,self.accuracy);
+        self.errors = cat(1,J,self.errors);
 
-        newWeights(sample,:) = w;
-        newBias(sample,:) = b(1,:);
-        
+        [w, b] = backprop2(self, prediction, target, self.images(sample,:));
+        b = mean(b,2);
+        newWeights(counter,:) = w;
+        newBias(counter,:) = b;
+
         % update according to batchSize
         % the below enables the use of SGD, batch, and various sizes of
         % minibatches
-        if mod(batchSize,sample) == 0
+        if mod(sample,batchSize) == 0 && batchSize > 1 && sample ~= 1
+            newWeights = mean(cellfun(@(x)mean(x(:,1)),newWeights));
+            newBias = mean(newBias);
+            
             update2(self, newWeights, newBias);
-            newWeights = {};
-        else
-            [~, cols] = size(newWeights);
-            for c = 1:cols
-                newWeights{c} = mean(cat(1,newWeights{:,1}));
-                newBias(c) = mean(newBias(c));
-            end
+            
+            % reset batch memory
+            %newWeights = {};
+            newWeights = cell(batchSize, layers);
+            newBias = zeros(batchSize, layers);
+            counter = 0;
         end
         
         % keep track of percentage done
-        percent = 100 * (sample/sampleSize);
-        strPer = [num2str(percent) '%%'];
-        if percent < 100
-            strCR = [repmat('\b',1,length(strPer)-1)];
-        else
-            strCR = [repmat('\b',1,length(strPer)-1) '\b\b'];
-            strPer = [strPer '\n'];
+        if mod(sample,k) == 0
+            percent = 100 * (sample/sampleSize);
+            strPer = ['  ' num2str(percent) '%%'];
+            if percent < 100
+                strCR = [repmat('\b',1,length(strPer)-1)];
+            else
+                strCR = [repmat('\b',1,length(strPer)-1) '\b\b'];
+                strPer = [strPer '\n'];
+            end
+            fprintf([strCR strPer]);
         end
-        fprintf([strCR strPer]);
-        
-        
-        
     end  % end of sample set
 end % end of function
