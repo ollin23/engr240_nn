3b13d2f7bb7aa08f17fe8c6d90f4f938c36917fb
ollin23 <balam.ariya@gmail.com>
2019-11-14 00:40:23 -0800

    update
    - tweaking assessment for accuracy and precision
    - correct formula for L1 and L2 regularization

--- train2.m
+++ train2.m
@@ -1,4 +1,4 @@
-function train2(self)
+function [err, acc, prec] = train2(self)
 %train executes feedforward, backpropagation, and updates the biases and
 % weights of neural network
 
@@ -16,23 +16,37 @@ function train2(self)
     end
     layers = length(self.weights);
     
-    % allocate space for error, new weights, and bias
-    J = 0;
+    % allocate space for error, new weights, new biases, running accuracy
+    % and running precision
     newWeights = cell(batchSize, layers);
     newBias = zeros(batchSize,layers);
+    runningError = [];
+    runningAccuracy = [];
+    runningPrecision = [];
+
+    epochError = [];
+    epochAccuracy = [];
+    epochPrecision = [];
     
+    % helper code for displaying percentage complete
     k = 2;
     if sampleSize > 1000
         k = sampleSize / 1000;
     end
     counter = 0;
+    
+    % * * * * * * * * * * * * * * * * * * * 
     % begin cycling through the samples
+    % * * * * * * * * * * * * * * * * * * * 
     for sample = 1:sampleSize
         counter = counter + 1;
         self.memory = {};
         input = self.images(sample,:);
         target = self.encodedLabels(sample,:);
-        %feedforward
+        label = self.labels(sample);
+
+        % * * * * * * *
+        % FEEDFORWARD
         for l = 1:layers
             h = feedforward2(self, l, input);
             
@@ -50,23 +64,95 @@ function train2(self)
             % store output as next layers input
             input = a;
             
+            % save last layer output as prediction
+            if l == layers
+                prediction = a;
+            end
         end % end of layers
+        % * * * * * * * * * * * *
+        % Calculate Error
+        % * * * * * * * * * * * *
+        J = cost(self.costFunction, prediction, target);
 
-        % calculate error
-        [rows, cols] = size(self.memory);
-        prediction = reshape([self.memory{:,cols}],[],rows)';
+        if ~(self.optim.none)
+            if self.optim.lasso
+                L1norm = 0;
+                for w = 1:length(self.weights)
+                    L1norm = L1norm + norm(self.weights{w});
+                end
+                J = J + ...
+                     (self.lambda / numel(self.weights) * ...
+                     L1norm;
+%                     sum(cellfun(@(x)sum(x(:)),self.weights));
+            end
+            if self.optim.ridge
+                L2norm = 0;
+                for w = 1:lenth(self.weights)
+                    L2norm = L2norm + norm(self.weights{w}.^2);
+                end
+                J = J + ...
+                     (self.lambda / length(self.labels)) * ...
+                     L2norm;
+%                     sum(cellfun(@(x)sum(x(:).^2),self.weights));
+            end
+        end
+        
 
-        J = cost('cross', prediction, target);
-        acc = dot(target,prediction);
-                    
-        self.accuracy = cat(1,acc,self.accuracy);
-        self.errors = cat(1,J,self.errors);
+        % calculate accuracy and precision
+        p = round((prediction - min(prediction))/(max(prediction) - min(prediction)));
+        
+%         [~,c] = max(prediction);
+%         oPred = oneHotEncoding(1:length(target));
+%         p = oPred(c,:);
+%         
+  
+        positives = nnz(p);
+        if positives > 0
+            diff = (target - p);
+            FP = nnz(diff<0);
+            FN = nnz(diff>0);
+            TN = sum(p == diff);
+            TP = abs(1-FN);
+            pLabel = mod(find(p>0),length(target));
+        else
+            FP = 0;
+            TP = 0;
+            FN = 1;
+            TN = 9;
+            pLabel = -1;
+        end
+        
+        %acc = (pLabel == label);
+       
+        
+%          fprintf('\nlabel :      %d\n',label);
+%          fprintf('\nprediction : %d\n',pLabel);
+%          pause();
+
+        acc = (TP + FP)/(FP + TP + FN + TN);
+        prec = TP / (TP + FP);
+%         fprintf('accuracy:\n');
+%         fprintf('\t(TP + FP)/(FP + TP + FN + TN)\n');
+%         fprintf('\t(%d + %d)/(%d + %d + %d + %d) = %0.4f\n',TP,FP,FP,TP,FN,TN,acc); 
+%         fprintf('precision:\n');
+%         fprintf('\tTP / (TP + FP)\n');
+%         fprintf('\t%D / (%d + %d) = %0.4f\n',TP, TP,FP,prec);
+%         fprintf('Error : %0.5f\n',J);
+%         pause();
+
+        % append precision, accuracy, and error
+        runningPrecision = cat(1,prec,runningPrecision);
+        runningAccuracy = cat(1,acc,runningAccuracy);
+        runningError = cat(1,J,runningError);
 
         [w, b] = backprop2(self, prediction, target, self.images(sample,:));
         b = mean(b,2);
         newWeights(counter,:) = w;
         newBias(counter,:) = b;
-
+        
+        % * * * * * * * * * * * * * *
+        %            UPDATE
+        % * * * * * * * * * * * * * *
         % update according to batchSize
         % the below enables the use of SGD, batch, and various sizes of
         % minibatches
@@ -74,13 +160,24 @@ function train2(self)
             newWeights = mean(cellfun(@(x)mean(x(:,1)),newWeights));
             newBias = mean(newBias);
             
+            
+            err = mean(runningError);
+            acc = mean(runningAccuracy);
+            prec = mean(runningPrecision);
+                        
+            epochPrecision = cat(1,prec,epochPrecision);
+            epochAccuracy = cat(1,acc,epochAccuracy);
+            epochError = cat(1,err,epochError);
+            
             update2(self, newWeights, newBias);
             
             % reset batch memory
-            %newWeights = {};
             newWeights = cell(batchSize, layers);
             newBias = zeros(batchSize, layers);
             counter = 0;
+            runningPrecision = [];
+            runningAccuracy = [];
+            runningError = [];
         end
         
         % keep track of percentage done
@@ -88,7 +185,7 @@ function train2(self)
             percent = 100 * (sample/sampleSize);
             strPer = ['  ' num2str(percent) '%%'];
             if percent < 100
-                strCR = [repmat('\b',1,length(strPer)-1)];
+                strCR = repmat('\b',1,length(strPer)-1);
             else
                 strCR = [repmat('\b',1,length(strPer)-1) '\b\b'];
                 strPer = [strPer '\n'];
@@ -96,4 +193,14 @@ function train2(self)
             fprintf([strCR strPer]);
         end
     end  % end of sample set
+    
+    % return error, accuracy, and precision
+    err = mean(epochError);
+    acc = mean(epochAccuracy);
+    prec = mean(epochPrecision);
+    
+    self.errors = cat(1,err,self.errors);
+    self.accuracy = cat(1,acc,self.accuracy);
+    self.precision = cat(1,prec,self.precision);
+
 end % end of function
