e4f299257e6c7c55519b7c114e9bb04b60d41446
ollin23 <balam.ariya@gmail.com>
2019-11-14 03:14:22 -0800

    minor update to mach train2.m changes

--- update2.m
+++ update2.m
@@ -1,11 +1,16 @@
 function update2(self, deltaWeights, deltaBias)
 % update updates the weights and biases with the changes calculated during
 % backpropagation
+%
+% PARAMETERS
+% self
+% deltaWeights
+% deltaBias
 
 
 for l = 1:length(self.weights)
-    self.weights{l} = self.weights{l} + deltaWeights{l};
-    self.bias(l) = self.bias(l) + deltaBias(l);
+    self.weights{l} = self.weights{l} - deltaWeights{l};
+    self.bias(l) = self.bias(l) - deltaBias(l);
 end
 
 end
