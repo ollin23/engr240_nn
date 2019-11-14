75e9e1930cb9b45498bcd446d4ffbf36c4cdcb11
ollin23 <balam.ariya@gmail.com>
2019-11-13 17:57:35 -0800

    updated plotting

--- fit.m
+++ fit.m
@@ -1,47 +1,72 @@
 function [] = fit(self)
 %fit executes the training cycle
 %
-
-    cycle = round(floor(.1* self.epochs),-1);
     ep = 0;
     x = 1:self.epochs;
-    y1 = [];
-    y2 = [];
     
-    for epoch = x
+    %cycle = round(self.epochs  / 10);
+
+    for epoch = 1:self.epochs
         ep = ep + 1;
-        %train(net);
-        fprintf('Epoch %d:       ',epoch);
+        
+%         self.errors = [];
+%         self.accuracy = [];
+        
+        %randomize images
+        [r,~] = size(self.images);
+        shuffledIndex = randperm(r);
+        self.images(1:end,:) = self.images(shuffledIndex,:);
+        
+        fprintf('Epoch %d:         ',epoch);
         
         % start timer
         tic;
-        train2(self);
-        err = self.errors(end,2);
-        loss = self.errors(end,1);
+        % train the network
+        [err, acc, prec] =train2(self);
         
-        fprintf('\tError %0.10f\n',err);
-        fprintf('\tTime Elapsed: %0.10f\n',toc);
+%         fprintf('\nepoch = %d\n',epoch);
+%         fprintf('\nlength(self.errors) = %d\n',length(self.errors));
+%         err = self.errors(epoch-1);
+%         acc = self.accuracy(epoch-1);
+
+        fprintf('\tError :       %0.5f\n',err);
+        fprintf('\tAccuracy :    %0.5f\n',acc);
+        fprintf('\tPrecision :   %0.5f\n',prec);
+        
+        fprintf('\tTime Elapsed: %0.5f\n',toc);
 %         % print after each 10% of epochs finish
 %         if epoch == 1
-%             fprintf('Epoch %d : Error %0.10f\n',epoch, err);
+%             fprintf('Epoch %d :\n',epoch);
+%             fprintf('\tError :\t\t %0.5f\n',self.errors(ep));
+%             fprintf('\tAccuracy :\t  %0.5f\n',self.accuracy(ep));
 %         elseif (mod(epoch,cycle) == 0)
-%             fprintf('Epoch %d : Error %0.10f\n',epoch, err);
+%             fprintf('Epoch %d :\n',epoch);
+%             fprintf('\tError :\t\t %0.5f\n',self.errors(ep));
+%             fprintf('\tAccuracy :\t  %0.5f\n',self.accuracy(ep));
 %         elseif (epoch == self.epochs)
-%             fprintf('Epoch %d : Error %0.10f\n',epoch, err);
+%             fprintf('Epoch %d :\n',epoch);
+%             fprintf('\tError :\t\t %0.5f\n',self.errors(ep));
+%             fprintf('\tAccuracy :\t  %0.5f\n',self.accuracy(ep));
 %         end
-        y1 = cat(1,err,y1);
-        y2 = cat(1,loss,y2);
-        if err <= 0
-            break;
-        end
 
     end
     
-    subplot(2,1,1)
-    plot(x,y1),xlabel('Epochs'),ylabel('Error');
-    title(['Accuracy ',num2str(ep),' Epochs']);
+   
+    set(gcf, 'Position', [100 100  400 750]);
+    y1 = flip(self.errors);
+    y2 = flip(self.accuracy);
     
-    subplot(2,1,2)
-    plot(x,y2),xlabel('Epochs'),ylabel('Loss');
-    title(['Loss Over ',num2str(ep),' Epochs']);
+    
+    subplot(3,1,1)
+    plot(x,y1),xlabel('Epochs'),ylabel('Error');
+    title([self.costFunction,' Error Over Time ',num2str(self.epochs),' Epochs']);
+
+    subplot(3,1,2)
+    plot(x,y2),xlabel('Epochs'),ylabel('Accuracy');
+    title(['Accuracy Over ',num2str(self.epochs),' Epochs']);
+    
+    subplot(3,1,3)
+    plot(x,y3),xlabel('Epochs'),ylabel('Precision');
+    title(['Precision Over ',num2str(self.epochs),' Epochs']);
+    
 end
